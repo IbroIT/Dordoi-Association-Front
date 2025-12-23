4bb6a67bc5b127f79d011c2d64b00f307fd7ffcd
IbroIT <ibroabdraimov20@gmail.com>
2025-12-23 12:53:20 +0600

    sdaf

--- src/components/pages/home/HomePage.jsx
+++ src/components/pages/home/HomePage.jsx
@@ -6,7 +6,7 @@ import AchievementsSection from './AchievementsSection'
 import ActivitiesSection from './ActivitiesSection'
 import NewsAndEventsSection from './NewsAndEventsSection'
 import InternationalPartnersSection from './InternationalPartnersSection'
-
+import PartnersSection from './PartnersSection'
 function HomePage() {
   return (
     <div>
@@ -17,6 +17,8 @@ function HomePage() {
         <MissionAndValues />
         {/* <AchievementsSection /> */}
         <ActivitiesSection />
+        <PartnersSection />
+
         {/* <InternationalPartnersSection /> */}
     </div>
   )
